294bb96f24ac2b27058135b76b54152f2f03ff9e
Rubem Silva <rubem.edu@outlook.com>
2021-08-04 04:37:39 -0300

    Implementei Identity no projeto

--- CleanArchMVC.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMVC.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using CleanArchMVC.Domain.Entities;
+﻿ using CleanArchMVC.Domain.Entities;
+using CleanArchMVC.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchMVC.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
